73c1b73f1c13dd393e7003e96750fb21b66283ef
burrowingowl <quminzhi@gmail.com>
2025-06-21 13:41:03 +0800

    sync

--- list/list.go
+++ list/list.go
@@ -11,15 +11,87 @@ func reverseList(head *ListNode) *ListNode {
 		return head
 	}
 
-	prev, next := head, head.Next
+	cur, next := head, head.Next
 	for next != nil {
 		tmp := next.Next
-		next.Next = prev
-		prev = next
+		next.Next = cur
+		cur = next
 		next = tmp
 	}
 	head.Next = nil
-	head = prev
+	head = cur
 
 	return head
+}
+
+// hasCycle detects if a linked list has a circle in it
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+
+	fast, slow := head, head
+	for fast != nil {
+		slow = slow.Next
+		fast = fast.Next
+		if fast == nil {
+			return false
+		}
+		fast = fast.Next
+		if fast == slow {
+			return true
+		}
+	}
+
+	return false
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		for next != nil && next.Val == cur.Val {
+			next = next.Next
+		}
+		cur.Next = next
+		cur = next
+	}
+	return head
+}
+
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	cur := head
+	for ; cur != nil && cur.Val == val; cur = cur.Next {
+	}
+
+	head, prev := cur, cur
+	for ; cur != nil; cur = cur.Next {
+		if cur.Val != val {
+			prev = cur
+		} else {
+			prev.Next = cur.Next
+		}
+	}
+	return head
+}
+
+func middleNode(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	mid, last := head, head
+	for mid != nil && last != nil && last.Next != nil {
+		mid = mid.Next
+		last = last.Next.Next
+	}
+	return mid
 }
