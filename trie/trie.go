24fbb0973aa030a2343bff6ecb03c4a347583c43
burrowingowl <quminzhi@gmail.com>
2025-07-30 12:15:32 +0800

    Sync

--- trie/trie.go
+++ trie/trie.go
@@ -48,3 +48,62 @@ func (this *Trie) StartsWith(prefix string) bool {
 	}
 	return true
 }
+
+type XORNode struct {
+	End      bool
+	Children [2]*XORNode
+}
+
+type XORTrie struct {
+	root *XORNode
+}
+
+func NewXORTrie() *XORTrie {
+	return &XORTrie{root: &XORNode{}}
+}
+
+func (this *XORTrie) Insert(num int) {
+	p := this.root
+	for i := 30; i >= 0; i-- {
+		t := num >> i & 1
+		if p.Children[t] == nil {
+			p.Children[t] = &XORNode{}
+		}
+		p = p.Children[t]
+	}
+	p.End = true
+}
+
+func (this *XORTrie) Search(num int) int {
+	res := 0
+	p := this.root
+	for i := 30; i >= 0; i-- {
+		t := num >> i & 1
+		if p.Children[t^1] != nil {
+			res += 1 << i
+			p = p.Children[t^1]
+		} else if p.Children[t] != nil {
+			p = p.Children[t]
+		} else {
+			break
+		}
+	}
+	return res
+}
+
+// 421
+func findMaximumXOR(nums []int) int {
+	trie := NewXORTrie()
+	maxXOR := 0
+	maxInt := func(x, y int) int {
+		if x > y {
+			return x
+		}
+		return y
+	}
+	for _, num := range nums {
+		maxXOR = maxInt(maxXOR, trie.Search(num))
+		trie.Insert(num)
+	}
+	return maxXOR
+}
